fdc488e450e03914dc6aeb1c3110f6bc63b8bfc6
Gunes Cagin Aydin <besney1k@gmail.com>
2026-03-27 17:57:29 +0000

    create repo

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Dynamical Metalearning';
+export const SITE_DESCRIPTION = 'Details on the work titled "Dynamical Metalearning: A Comparison Between Different Neural Architectures"';
